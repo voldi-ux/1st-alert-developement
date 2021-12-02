eefdc65cc933cd23639bfc9227cf176c39f106c0
voldi <68856495+voldi-ux@users.noreply.github.com>
2021-12-02 22:44:15 +0200

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,65 @@
+
+$('document').ready(function() {
+  $(".owl-carousel").owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 30,
+    responsive: {
+      780: {
+        items: 1,
+      },
+    },
+  });
+});
+
+$(".myCarousel").carousel({
+  interval:1000
+});
+
+$('document').ready(function() {
+  $("a[data-role='smoothscroll']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top - nav_height;
+
+    $("body, html").animate({
+        scrollTop: position
+    }, 1000 );
+    return false;
+  });
+});
+
+$('document').ready(function() {
+  // Back to top
+  var backTop = $(".back-to-top");
+
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+
+
+$('document').ready(function() {
+  // Tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Popovers
+  $('[data-toggle="popover"]').popover();
+
+  // Page scroll animate
+  new WOW().init();
+});
+
